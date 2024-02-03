bbf4c7b89903ae8e44bf72e5e8cdafe1615d85f8
mehdi_hosseini <mehdi1997hosseini@gmail.com>
2024-02-03 19:24:02 +0330

    create project and first commit

---
+++ workOutSadad/src/main/java/com/example/workoutsadad/WorkOutSadadApplication.java
@@ -0,0 +1,13 @@
+package com.example.workoutsadad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOutSadadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkOutSadadApplication.class, args);
+    }
+
+}
---
+++ workOutSadad/src/test/java/com/example/workoutsadad/WorkOutSadadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workoutsadad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOutSadadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
